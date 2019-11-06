bd9b298aaaffbd660bd75dde26eaf16970997193
ANDRES EMANUEL TASO <33707409@ort>
2019-11-05 21:23:09 -0300

    sube archivo

--- integrador/uploadImage/servidor.js
+++ integrador/uploadImage/servidor.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
-
+const fileUpload = require('express-fileupload')
 
 
 app.use(express.static('public'))
+app.use(fileUpload())
+
+
+app.post('/upload',(req,res) => {
+  let EDFile = req.files.file
+  EDFile.mv(`./public/files/${EDFile.name}`,err => {
+      if(err) return res.status(500).send({ message : err })
+
+      return res.status(200).send({ message : 'File upload' })
+  })
+})
+
+
 
 app.listen(3000, function(){
   console.log('Servidor iniciado');
-});
-
-app.post('/', (req, res) =>)
+})
