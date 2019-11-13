e58fddf7ca3ad31108106904fa012b230386e421
ANDRES EMANUEL TASO <33707409@ort>
2019-11-13 19:18:29 -0300

    separado

--- integrador/uploadImage/public/routes/cargarArchivo.js
+++ integrador/uploadImage/public/routes/cargarArchivo.js
@@ -2,13 +2,14 @@
 const express = require('express')
 const app = express();
 const fileUpload = require('express-fileupload')
+const router = require('express').Router()
 
 
 app.use(express.static('public'))
 app.use(fileUpload())
 
 //const baseURI = './public/fotos/'
-app.post('/upload',(req,res) => {
+router.post('/upload',(req,res) => {
   let EDFile = req.files.file
   EDFile.mv(`./public/fotos${EDFile.name}`,err => {
       if(err) return res.status(500).send({ message : err })
@@ -17,6 +18,5 @@ app.post('/upload',(req,res) => {
   })
 })
 
-app.listen(8080, function(){
-  console.log('Servidor iniciado');
-})
+
+module.exports= router, app
--- integrador/uploadImage/servidor.js
+++ integrador/uploadImage/servidor.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
 
-//const cargaFotos = require('./public/routes/cargarArchivo')
-//app.use('./api/public',cargaFotos)
+const cargaFotos = require('./public/routes/cargarArchivo')
+app.use('./api',cargaFotos)
 //const baseFotos = '/public/fotos'
-const fileUpload = require('express-fileupload')
+//const fileUpload = require('express-fileupload')
 app.use(express.static('public'))
-app.use(fileUpload())
+//app.use(fileUpload())
 
-const baseURI = './public/fotos/'
-app.post('/upload',(req,res) => {
-  let EDFile = req.files.file
-  EDFile.mv(`baseURI${EDFile.name}`,err => {
-      if(err) return res.status(500).send({ message : err })
 
-     return res.status(200).send({ message : 'Archivo subido correctamente' })
-  })
-})
+//app.post('/upload',(req,res) => {
+  //let EDFile = req.files.file
+  //EDFile.mv(`baseURI${EDFile.name}`,err => {
+    //  if(err) return res.status(500).send({ message : err })
+
+    // return res.status(200).send({ message : 'Archivo subido correctamente' })
+  //})
+//})
 const mostrarFoto = require('./public/routes/mostrarFoto')
 app.use( './api/fotos',mostrarFoto)
 
